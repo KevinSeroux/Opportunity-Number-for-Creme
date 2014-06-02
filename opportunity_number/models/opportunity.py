d1e67c90324d273692a3ad8e9b9fe38d19a9ee79
Saggah <kevin.seroux@orange.fr>
2014-06-02 09:33:18 +0200

    Imports sorted, hooks declarations is better in creme_core_register.py
    http://www.cremecrm.com/forum/viewtopic.php?p=843#p843

--- opportunity_number/models/opportunity.py
+++ opportunity_number/models/opportunity.py
@@ -2,13 +2,12 @@
 # Inspired from http://www.cremecrm.com/forum/viewtopic.php?f=6&t=563&start=10#p629
 
 from django.db.models import Model, PositiveIntegerField
-from creme.opportunities.models.opportunity import Opportunity
-from creme.creme_core.utils.contribute_to_model import contribute_to_model
 from django.utils.translation import ugettext_lazy as _
+from creme.creme_core.utils.contribute_to_model import contribute_to_model
+from creme.opportunities.models.opportunity import Opportunity
 
 class CustomOpportunity(Model):
-    # If you want to process to a datas migration set unique=False while you're setting,
-    # opportunity numbers, then restore it
+    #If you want to process to a datas migration set unique=True while you're setting opportunity numbers
     opportunity_number = PositiveIntegerField(_("Opportunity number"), unique=True)
 
     class Meta:
